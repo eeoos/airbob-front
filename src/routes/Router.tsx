441b0adfc5a61d2787f7bb3960a51a6bc191867d
jaehoonchoi <eeoos8840@gmail.com>
2026-07-02 15:13:43 +0900

    refactor: centralize route path contracts

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -14,26 +14,27 @@ import PaymentFail from "../pages/Reservations/PaymentFail";
 import Login from "../pages/Auth/Login/Login";
 import Signup from "../pages/Auth/Signup/Signup";
 import NotFound from "../pages/NotFound/NotFound";
+import { ROUTE_PATHS } from "./paths";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/accommodations/:id" element={<AccommodationDetail />} />
-        <Route path="/accommodations/:id/confirm" element={<ReservationConfirm />} />
-        <Route path="/accommodations/:id/edit" element={<AccommodationEdit />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/profile/host/reservations/:reservationUid" element={<HostReservationDetail />} />
-        <Route path="/reservations/:reservationUid" element={<ReservationDetail />} />
-        <Route path="/reservations/:reservationUid/review" element={<ReviewCreate />} />
-        <Route path="/reservations/:reservationUid/success" element={<PaymentSuccess />} />
-        <Route path="/reservations/:reservationUid/fail" element={<PaymentFail />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path={ROUTE_PATHS.home} element={<Home />} />
+        <Route path={ROUTE_PATHS.search} element={<Search />} />
+        <Route path={ROUTE_PATHS.accommodationDetail} element={<AccommodationDetail />} />
+        <Route path={ROUTE_PATHS.accommodationConfirm} element={<ReservationConfirm />} />
+        <Route path={ROUTE_PATHS.accommodationEdit} element={<AccommodationEdit />} />
+        <Route path={ROUTE_PATHS.wishlist} element={<Wishlist />} />
+        <Route path={ROUTE_PATHS.profile} element={<Profile />} />
+        <Route path={ROUTE_PATHS.hostReservationDetail} element={<HostReservationDetail />} />
+        <Route path={ROUTE_PATHS.reservationDetail} element={<ReservationDetail />} />
+        <Route path={ROUTE_PATHS.reviewCreate} element={<ReviewCreate />} />
+        <Route path={ROUTE_PATHS.paymentSuccess} element={<PaymentSuccess />} />
+        <Route path={ROUTE_PATHS.paymentFail} element={<PaymentFail />} />
+        <Route path={ROUTE_PATHS.login} element={<Login />} />
+        <Route path={ROUTE_PATHS.signup} element={<Signup />} />
+        <Route path={ROUTE_PATHS.notFound} element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
